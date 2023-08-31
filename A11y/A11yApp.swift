68681b4520b75d6890eb119e5d8ce0dcdc2e2e15
Alex Bonder <lexbonder@gmail.com>
2023-08-31 07:56:53 -0400

    Initial Commit

---
+++ A11y/A11yApp.swift
@@ -0,0 +1,17 @@
+//
+//  A11yApp.swift
+//  A11y
+//
+//  Created by Alex Bonder on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct A11yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
